729608d322cfe910036b59ad6a857739fb0166f2
Nikita Pochaev <pochaev.nik@gmail.com>
2026-08-11 09:04:32 +0400

    Integrate Detekt checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,19 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.detekt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
+}
+
+subprojects {
+    apply(plugin = "dev.detekt")
+
+    extensions.configure<dev.detekt.gradle.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+        basePath.set(rootProject.projectDir)
+    }
 }
